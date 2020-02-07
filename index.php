80ed0448d4158d190c1d172eb56dc6e954e761c3
Miguel Soares <miguel.soares@cpnv.ch>
2020-02-07 09:35:34 +0100

    update test

--- index.php
+++ index.php
@@ -6,5 +6,59 @@
  * Time: 13:37
  */
 
+function getAllItems()
+{
+    require ".constant.php";
+    try {
+
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+        $query = 'SELECT * FROM filmmakers';
+        $statment = $dbh->prepare($query);//prepare query
+        $statment->execute();//execute query
+        $queryResult = $statment->fetchAll();//prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+}
+
+function getActorsItem(){
+    require ".constant.php";
+    try {
+
+        $dbh = new PDO('mysql:host=' . $dbhost . ';dbname=' . $dbname, $user, $pass);
+        $query = 'SELECT * FROM actors';
+        $statment = $dbh->prepare($query);//prepare query
+        $statment->execute();//execute query
+        $queryResult = $statment->fetchAll();//prepare result for client
+        $dbh = null;
+        return $queryResult;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        return null;
+    }
+}
+
+//Test unitaire de la fonction getAllItems
+$items=getAllItems();
+if(count($items)==4){
+    echo 'OK Funtion getAllItems!!!';
+}
+else
+{
+    echo 'BUG !!!';
+}
+
+//Test unitaire de la fonction getActorstems
+$items=getActorsItem();
+if(count($items)==11){
+    echo 'OK Function getActorsItems !!!';
+}
+else
+{
+    echo 'BUG !!!';
+}
 ?>
 
